8a9d835811f6e154b8d3ffb78617c4786b4f80ce
JulienOza <julien.ozarowski@gmail.com>
2024-02-13 12:07:04 +0100

    typo fix

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <h1>Hello Wolrd </h1>
-  </React.StrictMode>,
-)
+    <h1>Hello World </h1>
+  </React.StrictMode>
+);
